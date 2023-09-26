b1b68c369c8eb769120608f4e33c5b73f808dd18
jinWan0823 <dhksl823@gmail.com>
2023-09-27 01:00:22 +0900

    ✨ Intro infinite scroll 구현

---
+++ src/intro/ImgSlide.tsx
@@ -0,0 +1,112 @@
+import styled, { keyframes } from "styled-components";
+import Rent from "../assets/rent.png";
+import FaceMaker from "../assets/facemaker.png";
+import Arum from "../assets/arum.png";
+import TripTogether from "../assets/triptogether.png";
+import Woori from "../assets/woori.png";
+
+import { useState } from "react";
+
+const slides = [FaceMaker, Arum, Rent, TripTogether, Woori];
+
+export default function ImgSlide() {
+  const [animate, setAnimate] = useState(true);
+  const onStop = () => setAnimate(false);
+  const onRun = () => setAnimate(true);
+
+  return (
+    <Wrapper>
+      <div className="slide-container">
+        <ul
+          className="slide-wrapper"
+          onMouseEnter={onStop}
+          onMouseLeave={onRun}
+        >
+          <div className={"slide original".concat(animate ? "" : " stop")}>
+            {slides.map((v, i) => (
+              <li key={i}>
+                <img src={v} alt="img-slide" />
+              </li>
+            ))}
+          </div>
+          <div className={"slide clone".concat(animate ? "" : " stop")}>
+            {slides.map((v, i) => (
+              <li key={i}>
+                <img src={v} alt="img-slide" />
+              </li>
+            ))}
+          </div>
+        </ul>
+      </div>
+    </Wrapper>
+  );
+}
+
+const infiniteAnimation1 = keyframes`
+  0% {
+    transform: translateX(0%);
+  }
+  50% {
+    transform: translateX(-100%);
+  }
+  50.1% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(0%);
+  }
+`;
+const infiniteAnimation2 = keyframes`
+  0% {
+    transform: translateX(0%);
+  }
+  100% {
+    transform: translateX(-200%);
+  }
+`;
+const Wrapper = styled.div`
+  margin: 40px 0px;
+  .slide-container {
+    overflow: hidden;
+  }
+  .slide-wrapper {
+    display: flex;
+    flex-wrap: nowrap;
+  }
+  .slide {
+    display: flex;
+    align-items: center;
+    flex-wrap: nowrap;
+    position: relative;
+    &::before {
+      content: "";
+      display: block;
+      width: 100%;
+      height: 1px;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      background-color: #bbb;
+      z-index: 1;
+    }
+    &.original {
+      animation: 45s linear infinite normal none running ${infiniteAnimation1};
+    }
+    &.clone {
+      animation: 45s linear infinite ${infiniteAnimation2};
+    }
+    &.stop {
+      animation-play-state: paused;
+    }
+
+    li {
+      margin: 0px 10px;
+      z-index: 2;
+      transition: 0.3s;
+      transform: scale(1);
+      cursor: pointer;
+      position: relative;
+    }
+  }
+`;
---
+++ src/intro/Intro.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import Title from "../commonComponents/title";
+import ImgSlide from "./ImgSlide";
+
+export default function Intro() {
+  return (
+    <section>
+      <Title>FRONTEND DEVELOPER</Title>
+      <ImgSlide />
+      <TitleWrap>
+        <h3>
+          ONE STEP FROM <br />
+          PUBLISHER TO <br />
+          FRONT-END DEVELOPER
+        </h3>
+        <Title>PORTFOLIO</Title>
+      </TitleWrap>
+    </section>
+  );
+}
+
+const TitleWrap = styled.div`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  h3 {
+    font-size: 32rem;
+    line-height: 1.1;
+    text-align: right;
+    margin-right: 110px;
+    letter-spacing: -1.2px;
+  }
+`;
