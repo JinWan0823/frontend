224f4e2f787236cc69d5ecd9da819b94b29e941e
jinWan0823 <dhksl823@gmail.com>
2023-10-05 18:51:15 +0900

    💄 풀 메뉴 UI , 애니메이션

---
+++ src/commonComponents/FullMenu.tsx
@@ -0,0 +1,148 @@
+import styled from "styled-components";
+
+interface Props {
+  toggleMenu: boolean;
+}
+
+export default function FullMenu({ toggleMenu }: Props) {
+  return (
+    <MenuOverlay className={toggleMenu ? "on" : ""}>
+      <nav className="menu-wrap">
+        <ul>
+          <li>
+            <a href="">ABOUT</a>
+          </li>
+          <li>
+            <a href="">PROJECT</a>
+          </li>
+          <li>
+            <a href="">CONTACT</a>
+          </li>
+        </ul>
+      </nav>
+    </MenuOverlay>
+  );
+}
+
+const MenuOverlay = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  opacity: 1;
+  visibility: hidden;
+  transition: opacity 0.5s, visibility 0.5s, width 0.5s;
+  z-index: 99;
+
+  &::before {
+    content: "";
+    background: #000;
+    position: absolute;
+    left: -55%;
+    top: 0;
+    width: 50%;
+    height: 100%;
+    transition: left 0.5s ease;
+  }
+  &::after {
+    content: "";
+    background: #000;
+    position: absolute;
+    right: -55%;
+    top: 0;
+    width: 50%;
+    height: 100%;
+    transition: all 0.5s ease;
+  }
+
+  &.on {
+    opacity: 0.95;
+    visibility: visible;
+    &::before {
+      left: 0;
+    }
+    &::after {
+      right: 0;
+    }
+    li {
+      animation: fadeInRight .5s ease forwards;
+      animation-delay: .5s;
+      
+      &:nth-of-type(2) {
+        animation-delay: .6s;
+      }
+      &:nth-of-type(3) {
+        animation-delay: .7s;
+      }
+    }
+  }
+  nav {
+    position: relative;
+    height: 60%;
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: 50px;
+    text-align: center;
+    z-index: 100;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0 auto;
+    display: inline-block;
+    position: relative;
+    height: 100%; 
+    font-family: "Oswald", sans-serif;
+    font-weight:800;
+    li {
+      display: flex;
+      align-items:center;
+      justify-content:center;
+      height: 33.333%;
+      height: calc(100% / 3);
+      min-height: 50px;
+      position: relative;
+      opacity: 0;
+      
+      a {
+        display: block;
+        position: relative;
+        color: #fff;
+        text-decoration: none;
+        font-size:110rem;
+        &:hover {
+          color:#b15e30
+        }
+        &:hover:after,
+         &:focus:after,
+         &:active:after {
+           width: 100%;
+         }
+        
+        &:after {
+          content: '';
+          position: absolute;
+          bottom: -5px;
+          left: 50%;
+          width: 0%;
+          transform: translateX(-50%);
+          height: 6px;
+          background: #b15e30;
+          transition: .35s;
+        }
+      }
+    }
+  }
+}
+
+@keyframes fadeInRight {
+  0% {
+    opacity: 0;
+    left: 20%;
+  }
+  100% {
+    opacity: 1;
+    left: 0;
+  }
+}`;
