912ab3c39f8e44f987b141a09062b6d0bb7c9883
jinWan0823 <dhksl823@gmail.com>
2023-10-05 16:56:11 +0900

    💄 링크버튼 / 캐러셀 이미지 UI

--- db.ts
+++ db.ts
@@ -1,14 +1,15 @@
-import Triptogether from "./src/assets/triptogether.png";
-import FaceMaker from "./src/assets/facemaker.png";
-import Arum from "./src/assets/arum.png";
-import OliveRent from "./src/assets/rent.png";
+import Triptogether from "./src/assets/slideImg/triptogether.png";
+import FaceMaker from "./src/assets/slideImg/facemaker.png";
+import Arum from "./src/assets/slideImg/karum.png";
+import OliveRent from "./src/assets/slideImg/rentcar.png";
+import Nengtul from "./src/assets/slideImg/nengtul.png";
 
 const ListData = {
   portfolio: [
     {
       id: 1,
       title: "TEAM NENGTUL",
-      thumb: Triptogether,
+      thumb: Nengtul,
       content:
         "냉장고를 털어줘 프로젝트는 사용자들이 자신이 가지고 있는 재료를 이용해 다른 사람들이 올린 레시피의 재료들과 매칭 시켜 음식 레시피를 추천해주는 서비스입니다. 추가로 음식 재료가 남는다면 주변 사람들과 공유할 수 있는 커뮤니티를 제공합니다.",
       date: "2023-07-08 ~ 2023-08-16",
@@ -17,27 +18,46 @@ const ListData = {
         "https://spice-expert-177.notion.site/5ecad1dcc16c4df5b81c18395a21e6bc?pvs=4",
       site: "https://nengtul-frontend-iota.vercel.app/",
       skill: "React / TypeScript / Redux-persist / Styled-components",
+      youtube: "https://www.youtube.com/watch?v=nAZUx73mwVU&t=71s",
     },
     {
       id: 2,
       title: "TRIP TOGETHER",
-      thumb: FaceMaker,
+      thumb: Triptogether,
       content: "~~~~~~~~~~~~~~~~~~~~~~~~",
+      skill: "React / TypeScript / TailwindCss / Firebase",
       date: "2023-06-01 ~ 2023-07-10",
-      gitub: "https://github.com/JinWan0823/tripTogether",
-      site: "",
+      github: "https://github.com/JinWan0823/tripTogether",
+      site: "https://www.youtube.com/watch?v=ekboLw2p_2M",
+      youtube: "https://www.youtube.com/watch?v=ekboLw2p_2M",
+      notion: "",
     },
     {
       id: 3,
       title: "FACE MAKER",
-      thumb: Arum,
+      thumb: FaceMaker,
+      skill: "JavaScript / Jquery / PHP / MySql",
       content: "~~~~~~~~~~~~~~~~~~~~~~~~",
+      date: "2022-08-10 ~ 2022-09-15",
+      site: "https://face-maker.co.kr/",
     },
     {
       id: 4,
       title: "OLIVE RENTCAR",
       thumb: OliveRent,
+      skill: "JavaScript / Jquery / PHP / MySql",
       content: "~~~~~~~~~~~~~~~~~~~~~~~~",
+      date: "2022-10-01 ~ 2022-11-01",
+      site: "https://dhksl823.iwinv.net/rent/",
+    },
+    {
+      id: 5,
+      title: "SUPER MODEL ARUM",
+      skill: "JavaScript / Jquery / PHP / MySql",
+      thumb: Arum,
+      date: "2022-06-10 ~ 2022-07-29",
+      content: "~~~~~~~~~~~~~~~~~~~~~~~~",
+      site: "https://ksarum.com/",
     },
   ],
 };
